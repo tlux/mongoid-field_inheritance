45cb8c520263fe23de32c54915dbc5ddc3a0babd
Tobias Casper <tobias.casper@gmail.com>
2015-03-01 03:52:22 +0100

    Adds documentation to UndefinedParentError

--- lib/mongoid/field_inheritance/undefined_parent_error.rb
+++ lib/mongoid/field_inheritance/undefined_parent_error.rb
@@ -1,8 +1,17 @@
 module Mongoid
   module FieldInheritance
+    ##
+    # An error to be raised when a document does not have a parent to inherit
+    # from.
+    #
+    # @since 0.1.0
     class UndefinedParentError < StandardError
       attr_reader :document
 
+      ##
+      # Initializes the exception.
+      #
+      # @param [Mongoid::Document] document The document without a parent.
       def initialize(document)
         @document = document
       end
