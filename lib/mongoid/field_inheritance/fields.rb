22427f497eb2f55c714da75c3502be9ead605f45
Tobias Casper <tobias.casper@gmail.com>
2015-03-02 19:28:59 +0100

    Adds more method docs

--- lib/mongoid/field_inheritance/fields.rb
+++ lib/mongoid/field_inheritance/fields.rb
@@ -14,15 +14,31 @@ module Mongoid
       module ClassMethods
         protected
 
+        ##
+        # Creates accessors for a field when loading the model class. This
+        # version hooks into the original implementation to create additional
+        # helper methods for field inheritance.
+        #
+        # @param [Symbol, String] name The name of the field.
+        # @param [Symbol] meth The name of the method.
+        # @option options [Boolean] :inherit Determines whether the field is
+        #   inheritable, which creates inheritance specific methods.
+        # @return [void]
         def create_accessors_with_inheritance(name, meth, options = {})
           create_accessors_without_inheritance(name, meth, options)
           create_inherited_check(name) if options[:inherit]
         end
 
+        ##
+        # Creates a name_inherited? method for each inheritable field, which
+        # internally calls the {#field_inherited?} method. Undefines methods
+        # of the same name, when existing.
+        #
+        # @param [Symbol, String] name The name of the field.
         def create_inherited_check(name)
           generated_methods.module_eval do
             re_define_method "#{name}_inherited?" do
-              attribute_inherited?(name)
+              field_inherited?(name)
             end
           end
         end
@@ -33,11 +49,11 @@ module Mongoid
       # given field is included in #inherited_fields of the current document.
       # Aliased as: #attribute_inherited?
       #
-      # @param [Symbol, String] field The name of the field.
+      # @param [Symbol, String] name The name of the field.
       # @return [Boolean] Returns true when the field is inherited,
       #   false otherwise.
-      def field_inherited?(field)
-        inherited_fields.include?(field.to_s)
+      def field_inherited?(name)
+        inherited_fields.include?(name.to_s)
       end
     end
   end
