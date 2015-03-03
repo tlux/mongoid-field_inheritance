7dcf5247b93bb37e0ea693fe0108d691848d3c75
Tobias Casper <tc@i22.de>
2015-03-03 14:48:27 +0100

    Removes default strategy spec context in propagation spec

--- spec/mongoid/field_inheritance/propagation_spec.rb
+++ spec/mongoid/field_inheritance/propagation_spec.rb
@@ -127,89 +127,6 @@ describe Mongoid::FieldInheritance::Propagation do
       end
     end
 
-    context 'with default strategy' do
-      let! :model do
-        ModelFactory.create_model do
-          include Mongoid::Timestamps::Updated
-
-          field :name, localize: true, inherit: :default
-          field :manufacturer, inherit: :default
-          field :sku
-        end
-      end
-
-      context 'with parent' do
-        let! :parent do
-          model.create manufacturer: 'Apple', name: 'iPhone', sku: '12345'
-        end
-
-        subject do
-          parent.children.new(inherited_fields: %w(name manufacturer))
-        end
-
-        it 'copies the inheritable fields from parent' do
-          expect { subject.save }.to(
-            change { subject.manufacturer }.from(nil).to('Apple')
-          )
-        end
-
-        it 'overwrites child-defined value if field is among inherited ' \
-           'fields' do
-          subject.manufacturer = 'Samsung'
-          expect { subject.save }.to(
-            change { subject.manufacturer }.from('Samsung').to('Apple')
-          )
-        end
-
-        it 'does not copy the uninheritable fields from parent' do
-          expect(subject.sku).to be nil
-        end
-
-        it 'does not overwrite uninheritable fields' do
-          subject.sku = '12345'
-          expect { subject.save }.to_not change { subject.sku }
-        end
-      end
-
-      context 'with children' do
-        subject do
-          model.create manufacturer: 'Apple', name: 'iPhone', sku: '12345'
-        end
-
-        let! :child do
-          subject.children.create inherited_fields: %w(manufacturer)
-        end
-
-        let! :grandchild do
-          child.children.create inherited_fields: %w(manufacturer)
-        end
-
-        it 'propagates to inherited attribute in child' do
-          expect { subject.update(manufacturer: 'Samsung') }.to change {
-            child.reload.manufacturer
-          }.from('Apple').to('Samsung')
-        end
-
-        it 'propagates to inherited attribute in grandchild' do
-          expect { subject.update(manufacturer: 'HTC') }.to change {
-            grandchild.reload.manufacturer
-          }.from('Apple').to('HTC')
-        end
-
-        it 'does not update inheriting child if nothing has changed' do
-          expect { subject.update(manufacturer: 'Apple') }.not_to change {
-            child.reload.updated_at
-          }
-        end
-
-        it 'does not propagate to non-inherited attributes of children' do
-          expect { subject.update(name: 'iPad') }.not_to change {
-            child.reload.name
-          }
-        end
-      end
-    end
-
     context 'with inherited embedded relation' do
       let! :embedded_model do
         ModelFactory.create_basic_model 'Property' do
@@ -239,7 +156,9 @@ describe Mongoid::FieldInheritance::Propagation do
           end
         end
 
-        subject { parent.children.new.tap(&:inherit) }
+        subject { parent.children.new }
+
+        before(:each) { subject.inherit }
 
         # it 'TODO' do
         # end
